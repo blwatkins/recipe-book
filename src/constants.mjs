ba54560893de9950c194514cb82c1e69dd6cd0df
blwatkins <blwatkins@users.noreply.github.com>
2025-12-23 21:56:05 -0600

    Update constants documentation.

--- src/constants.mjs
+++ src/constants.mjs
@@ -23,7 +23,7 @@
 /**
  * Get the trust proxy setting from environment variables.
  *
- * @return {number|boolean} Returns `true` for 'true', a number for valid numeric strings, or `false` for any other value or if not set.
+ * @return {number|boolean} `true` for 'true', a number for valid numeric strings, or `false` for any other value or if not set.
  */
 function getTrustProxy() {
     const trustProxy = process.env.TRUST_PROXY;
