95c37efa41bc72110385e89932fb34f0fedff6be
blwatkins <blwatkins@users.noreply.github.com>
2025-12-24 10:28:13 -0600

    #6 Add initial schema for recipes database.

---
+++ schema/unit_conversions.mysql.sql
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2025 brittni and the polar bear LLC.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+CREATE TABLE IF NOT EXISTS unit_conversions (
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    unit_from_id INTEGER NOT NULL,
+    unit_to_id INTEGER NOT NULL,
+    conversion_factor DOUBLE NOT NULL,
+    UNIQUE (unit_from_id, unit_to_id),
+    FOREIGN KEY (unit_from_id) REFERENCES units(id),
+    FOREIGN KEY (unit_to_id) REFERENCES units(id)
+);
